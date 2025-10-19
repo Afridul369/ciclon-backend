0ad0d1c77faa71c51dfd40a830493aea8a809f45
Afridul <afridul.dev@gmail.com>
2025-10-19 22:09:57 +0600

    Brand create get-all get-single delete-brand  Done

--- src/controller/brand.controller.js
+++ src/controller/brand.controller.js
@@ -2,8 +2,61 @@ const { asyncHandler } = require("../utils/asynchandeler");
 const { apiResponse } = require("../utils/apiResponse");
 const { customError } = require("../utils/customError");
 const {validateBrand} = require('../validation/brand.validation')
-const brandModel = require('../models/brand.model')
+const brandModel = require('../models/brand.model');
+const { uploadCloudinaryImage, deleteCloudinaryImage } = require("../helper/cloudinary");
 
+// create brand
+exports.CreateBrand = asyncHandler(async(req,res)=>{
+    const value = await validateBrand(req)
+    // console.log(value.image.path);
+    const imageUrl = await uploadCloudinaryImage(value?.image?.path)
+    if (!imageUrl) {
+        throw new customError(500, 'Image Not Created !!')
+    }
+    const brand = await brandModel.create(
+        {
+        ...value,
+        image: imageUrl
+        }
+    )
+    if (!brand) {
+        throw new customError(500,'Brand Not Created !!')
+    }
+    apiResponse.sendSucces(res,201,'Brand Created Successfully', brand)
+})
 
+// get all brand
+exports.GetAllBrand = asyncHandler(async(req,res)=>{
+    const allBrand = await brandModel.find()
+    if (!allBrand) {
+        throw new customError(500, 'Brand Not Found')
+    }
+    apiResponse.sendSucces(res,201,'brand Retrive Successfull', allBrand)
+})
 
+// get single brand
+exports.GetSingleBrand = asyncHandler(async(req,res)=>{
+    const {slug} = req.params
+    if (!slug) throw new customError(401, 'Please Insert Your Brand Name')
+    const brand = await brandModel.findOne({slug: slug})
+    if (!brand) throw new customError(500, 'Brand Not Found')
+    apiResponse.sendSucces(res,201,"Your Single Brand Retrive Successfull",brand)
+})
+
+// delete brand
+exports.DeleteBrand = asyncHandler(async(req,res)=>{
+    const {slug} = req.params
+    if (!slug) {throw new customError(401, 'Please Insert Your Brand Name')
+    }
+    const brand = await brandModel.findOneAndDelete({slug})
+    if (!brand) { throw new customError(500, 'Brand Not Found')
+    }
+// delete from cloudinary
+    const parts = brand.image.split('/')
+    const imageaPart = parts[parts.length - 1]
+    const imageId = imageaPart.split('?')[0]
+    const result = await deleteCloudinaryImage(imageId)
+     if (result !== "ok" ) throw new customError(400,"Image Not Deleted")
+    apiResponse.sendSucces(res,201,"Brand Delete Successfull",brand)    
+})
 
--- src/routes/api/brand.api.js
+++ src/routes/api/brand.api.js
@@ -1,7 +1,11 @@
 const exprees = require('express')
 const _ = exprees.Router()
 const BrandController = require('../../controller/brand.controller')
+const upload = require('../../middleware/multer.middleware')
 
-_.route('/')
+_.route('/create-brand').post(upload.fields([{name:'image',maxCount:1}]), BrandController.CreateBrand)
+_.route('/all-brand').get(BrandController.GetAllBrand)
+_.route('/single-brand/:slug').get(BrandController.GetSingleBrand)
+_.route('/delete-brand/:slug').delete(BrandController.DeleteBrand)
 
 module.exports = _
